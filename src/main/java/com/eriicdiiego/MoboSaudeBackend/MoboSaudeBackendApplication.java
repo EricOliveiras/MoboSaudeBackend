43c349ed0b48e593bc1717e4ea35b79692d46571
Eric Nascimento <oiericoi@hotmail.com>
2024-09-23 18:37:14 -0300

    build: init project

---
+++ src/main/java/com/eriicdiiego/MoboSaudeBackend/MoboSaudeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eriicdiiego.MoboSaudeBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoboSaudeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoboSaudeBackendApplication.class, args);
+	}
+
+}
